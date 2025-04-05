b65181df7a18be6ac74cf4ee87e7ea40eee9fea6
Richard Spindler <richard@lateralblast.com.au>
2025-04-05 18:47:17 +1100

    Replaced general help function

--- guige.sh
+++ guige.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Name:         guige (Generic Ubuntu/Unix ISO Generation Engine)
-# Version:      3.3.1
+# Version:      3.3.2
 # Release:      1
 # License:      CC-BA (Creative Commons By Attribution)
 #               http://creativecommons.org/licenses/by/4.0/legalcode
@@ -120,72 +120,90 @@ if [ "${script['args']}" = "" ]; then
   print_help
 fi
 
+# switchstart
+
 while test $# -gt 0
 do
   case "$1" in
     --action|--actions)
+      # Action to perform
       iso['action']="$2"
       shift 2
       ;;
-    --allow|--allowlist)
+    --allowlist|--allow)
+      # Allow/load additional kernel modules(s)
       iso['allowlist']="$2"
       shift 2
       ;;
     --allowpassword|--allow-password)
+      # Allow password access via SSH
       iso['allowpassword']="true"
       shift
       ;;
     --allowservice|--allowservices|--service|--services)
+      # Allow Services
       iso['allowservice']="$2"
       shift 2
       ;;
     --arch)
+      # Architacture
       iso['arch']="$2"
       shift 2
       iso['arch']=$( echo "${iso['arch']}" |sed "s/aarch64/arm64/g" |sed "s/x86_64/amd64/g" |sed "s/x86/amd64/g" )
       ;;
     --autoinstalldir|--aidir)
+      # Directory where autoinstall config files are stored on ISO
       iso['autoinstalldir']="$2"
       shift 2
       ;;
-    --block|--blocklist)
+    --blocklist|--block)
+      # Block kernel module(s)
       iso['blocklist']="$2"
       shift 2
       ;;
     --bmcip)
+      # BMC/iDRAC IP
       iso['bmcip']="$2"
       shift 2
       ;;
-    --bmcpass|--bmcpassword)
+    --bmcpassword|--bmcpass)
+      # BMC/iDRAC password
       iso['bmcpassword']="$2"
       shift 2
       ;;
-    --bmcuser|--bmcusername)
+    --bmcusername|--bmcuser)
+      # BMC/iDRAC User
       iso['bmcusername']="$2"
       shift 2
       ;;
-    --bootdisk|--disk|--installdisk|--firstdisk)
+    --disk|--bootdisk|--installdisk|--firstdisk)
+      # Boot Disk devices
       iso['disk']="$2"
       shift 2
       ;;
     --bootloader)
+      # Boot Loader Location
       iso['bootloader']="$2"
       shift 2
       ;;
     --bootserverfile)
+      # Boot sever file
       iso['bootserverfile']="$2"
       options['bootserverfile']="true"
       shift 2
       ;;
     --bootserverip)
+      # NFS/Bootserver IP
       iso['bootserverip']="$2"
       shift 2
       ;;
     --bootsize)
+      # Boot partition size
       iso['bootsize']="$2"
       shift 2
       ;;
     --build|--buildtype)
+      # Type of ISO to build
       iso['build']="$2"
       case "${iso['build']}" in
         "daily")
@@ -195,253 +213,314 @@ do
       shift 2
       ;;
     --chrootpackages)
+      # List of packages to add to ISO
       iso['chrootpackages']="$2"
       shift 2
       ;;
     --cidr)
+      # CIDR
       iso['cidr']="$2"
       shift 2
       ;;
     --codename|--distro)
+      # Linux release codename or distribution
       iso['codename']="$2"
       shift 2
       ;;
     --compression)
+      # Compression algorithm
       iso['compression']="$2"
       options['compression']="true"
       shift 2
       ;;
     --country)
+      # Country
       iso['country']="$2"
       shift 2
       ;;
     --debug)
+      # Set debug flag (set -x)
       set -x
       shift
       ;;
     --delete)
+      # Remove previously created files
       iso['delete']="$2"
       shift 2
       ;;
-    --disableservice|--disable)
+    --disableservice|--disableservices|--disable)
+      # Disable service(s)
       iso['disableservice']="$2"
       shift 2
       ;;
     --diskserial)
+      # Disk serial
       iso['diskserial']="$2"
       shift 2
       ;;
     --diskwwn)
+      # Disk WWN
       iso['diskwwn']="$2"
       shift 2
       ;;
     --dns|--nameserver)
+      # DNS server IP
       iso['dns']="$2"
       shift 2
       ;;
-    --enableservice|--enable)
+    --enableservice|--enableservices|--enable)
+      # Enable service(s)
       iso['enableservice']="$2"
       shift 2
       ;;
     --fallback)
+      # Installation fallback 
       iso['fallback']="$2"
       shift 2
       ;;
     --firewall)
+      # Firewall
       iso['firewall']="$2"
       shift 2
       ;;
     --firstoption|--first-option)
+      # First menu option (e.g. grub menu)
       iso['firstoption']="$2"
       shift 2
       ;;
     --gateway)
+      # Gateway IP
       iso['gateway']="$2"
       shift 2
       options['dhcp']="false"
       ;;
     --gecos)
+      # User GECOS field
       iso['gecos']="$2"
       shift 2
       ;;
     --groups)
+      # Groups to add user to
       iso['groups']="$2"
       shift 2
       ;;
-    --grub|--grubfile)
+    --grubfile|--grub)
+      # Import grub file
       options['grubfile']="true"
       iso['grubfile']="$2"
       shift 2
       ;;
     --grubmenu)
+      # Import grub menu
       iso['grubmenu']="$2"
       shift 2
       ;;
     --grubtimeout|--grub-timeout)
+      # Grub timeout
       iso['grubtimeout']="$2"
       shift 2
       ;;
-    -h|--help)
-      print_help ""
+    --help|-h)
+      # Print help
+      print_help "cli"
       ;;
     --hostname)
+      # Hostname
       iso['hostname']="$2"
       shift 2
       ;;
     --inputci|--vmci)
+      # Import Cloud Image
       iso['inputci']="$2"
       shift 2
       ;;
-    --inputiso|--vmiso)
+    --inputfile|--inputiso|--vmiso)
+      # Import ISO
       iso['inputfile']="$2"
       vm['inputfile']="$2"
       shift 2
       ;;
     --installmode|--install-mode)
+      # Install mode
       iso['installmode']="$2"
       shift 2
       ;;
     --installmount)
+      # Where the install mounts the CD during install
       iso['installmount']="$2"
       shift 2
       ;;
     --installpassword|--install-password|--installpass|--install-pass)
       iso['installpassword']="$2"
+      # Temporary install password for remote access during install
       shift 2
       ;;
     --installsource|--install-source)
+      # Install source
       iso['installsource']="$2"
       shift 2
       ;;
-    --installtarget)
+    --targetmount|--installtarget)
+      # Install target
       iso['targetmount']="$2"
       shift 2
       ;;
-    --installuser|--install-user)
+    --installusername|--installuser|--install-user)
+      # Install user
       iso['installusername']="$2"
       shift 2
       ;;
     --ip)
+      # IP address
       iso['ip']="$2"
       shift 2
       options['dhcp']="false"
       ;;
-    --isokernel|--kernel)
+    --kernel|--isokernel)
+      # Kernel to install
       iso['kernel']="$2"
       shift 2
       ;;
     --isokernelargs|--kernelargs)
+      # Kernel arguments
       iso['kernelargs']="$2"
       shift 2
       ;;
-    --isolinux|--isolinuxfile)
+    --isolinuxfile|--isolinux)
+      # Import isolinux file
       options['isolinuxfile']="true"
       iso['isolinuxfile']="$2"
       shift 2
       ;;
-    --isopackages|--packages)
+    --packages|--isopackages)
+      # Additional packages to install
       iso['packages']="$2"
       shift 2
       ;;
-    --isourl|--url)
+    --url|--isourl)
+      # ISO URL
       iso['url']="$2"
       shift 2
       ;;
-    --isovolid|--volid)
+    --volid|--isovolid)
+      # ISO Volume ID
       iso['volid']="$2"
       shift 2
       ;;
     --layout|--vmsize)
+      # Layout or VM disk size
       iso['layout']="$2"
       vm['size']=$2
       shift 2
       ;;
     --lcall)
+      # LC_ALL
       iso['lcall']="$2"
       shift 2
       ;;
     --locale)
+      # Local
       iso['locale']="$2"
       shift 2
       ;;
     --lvname)
+      # Logical Volume Name
       iso['lvname']="$2"
       shift 2
       ;;
     --netmask)
+      # Netmask
       iso['netmask']="$2"
       shift 2
       ;;
     --nic|--vmnic|--installnic|--bootnic|--firstnic)
+      # NIC to use for installation
       iso['nic']="$2"
       vm['nic']="$2"
       shift 2
       ;;
     --oeminstall)
+      # OEM Install
       iso['oeminstall']="$2"
       shift 2
       ;;
     --oldinputfile)
+      # Old release ISO
       old['inputfile']="$2"
       shift 2
       ;;
     --oldisourl)
+      # Old ISO URL
       old['url']="$2"
       shift 2
       ;;
     --oldrelease)
+      # Old release
       old['release']="$2"
       shift 2
       ;;
     --onboot)
+      # Enable network on boot
       iso['onboot']="$2"
       shift 2
       ;;
-    --option|--options)
+    --options|--option)
+      # Options (e.g. verbose)
       iso['options']="$2";
       shift 2
       ;;
     --outputci)
+      # Output CI file
       iso['outputci']="$2"
       shift 2
       ;;
-    --outputiso)
+    --outputfile|--outputiso)
+      # Output ISO file
       iso['outputfile']="$2"
       shift 2
       ;;
     --password)
+      # Password
       iso['password']="$2"
       shift 2
       ;;
     --passwordalgorithm|--password-algorithm|--passalgo|--algoritm)
+      # Password Algorithm
       iso['passwordalgorithm']="$2"
       shift 2
       ;;
     --pesize)
+      # PE size
       iso['pesize']="$2"
       shift 2
       ;;
     --postinstall)
+      # Import post install script
       iso['postinstall']="$2"
       shift 2
       ;;
     --prefix)
+      # Output file name prefix
       iso['prefix']="$2"
       shift 2
       ;;
     --preworkdir)
+      # Docker work directory
       iso['preworkdir']="$2"
       shift 2
       ;;
     --pvname)
+      # Physical Volume Name
       iso['pvname']="$2"
       shift 2
       ;;
     --realname)
+      # User real name field
       iso['realname']="$2"
       shift 2
       ;;
     --release)
+      # OS release
       iso['release']="$2"
       shift 2
       case ${iso['release']} in
@@ -457,108 +536,133 @@ do
       get_build_type
       ;;
     --rootsize)
+      # Root volume size
       iso['rootsize']="$2"
       shift 2
       ;;
     --search)
+      # Search output for value
       iso['search']="$2"
       shift 2
       ;;
     --selinux)
+      # SELinux Mode
       iso['selinux']="$2"
       shift 2
       ;;
     --serialport)
+      # Serial port
       iso['serialporta']="$2"
       shift 2
       ;;
     --serialportaddress)
+      # Serial port address
       iso['serialportaddressa']="$2"
       shift 2
       ;;
     --serialportspeed)
+      # Serial port speed
       iso['serialportspeeda']="$2"
       shift 2
       ;;
     --sourceid)
+      # Source ID
       iso['sourceid']="$2"
       shift 2
       ;;
     --squashfsfile)
+      # Squashfs file
       iso['squashfsfile']="$2"
       shift 2
       ;;
     --sshkey)
+      # SSH key
       iso['sshkey']="$2"
       options['sshkey']="true"
       shift 2
       ;;
     --sshkeyfile)
+      # SSH key file
       iso['sshkeyfile']="$2"
       options['sshkey']="true"
       shift 2
       ;;
     --suffix)
+      # Output file name suffix
       iso['suffix']="$2"
       shift 2
       ;;
     --swapsize|--vmram)
+      # Swap/RAM size
       iso['swapsize']="$2"
       VM_RAM="$2"
       shift 2
       ;;
     --timezone)
+      # Timezone
       iso['timezone']="$2"
       shift 2
       ;;
     --updates)
+      # Updates to install
       iso['updates']="$2"
       shift 2
       ;;
-    --userdata|--autoinstall|--kickstart)
+    --autoinstallfile|--userdata|--autoinstall|--kickstart)
+      # Import autoinstall config file
       options['autoinstall']="true"
       iso['volumemanager']="custom"
       iso['autoinstallfile']="$2"
       shift 2
       ;;
-    --user|--username)
+    --username|--user)
+      # Username
       iso['username']="$2"
       shift 2
       ;;
     --usage)
+      # Usage information
       print_usage "$2"
       exit
       ;;
     --version|-V)
+      # Display version
       echo "${script['version']}"
       shift
       exit
       ;;
     --vgname)
+      # Volume Group Name
       iso['vgname']="$2"
       shift 2
       ;;
-    --vmcpus)
+    --vmcpus|--cpus)
+      # Number of CPUs
       vm['cpus']="$2"
       shift 2
       ;;
-    --vmname)
+    --vmname|--name)
+      # VM name
       vm['name']="$2"
       shift 2
       ;;
-    --vmtype)
+    --vmtype|--type)
+      # VM type
       vm['type']="$2"
       shift 2
       ;;
     --volumemanager|--volumemanagers|--volmgr|--volmgrs)
+      # Volumemanager(s)
       iso['volumemanager']="$2"
       shift 2
       ;;
     --workdir)
+      # Work directory
       iso['workdir']="$2"
       shift 2
       ;;
-    --zfs|--zfsfilesystems)
+    --zfsfilesystems|--zfs)
+      # Additional ZFS filesystems
       options['zfsfilesystems']="true"
       iso['zfsfilesystems']="$2"
       shift 2
@@ -573,6 +677,8 @@ do
       ;;
   esac
 done
+
+# switchend
 
 # Setup functions
 
--- modules/help.sh
+++ modules/help.sh
@@ -9,111 +9,50 @@
 # Print script help information
 
 print_cli_help () {
-  cat <<-help
-
-  Usage: ${0##*/} [OPTIONS...]
-
-  --action(s)             Action to perform (e.g. createiso, justiso, runchrootscript, checkdocker, installrequired)
-  --allow                 Load additional kernel modules(s)
-  --allowpassword         Allow password access via SSH (default: ${defaults['allowpassword']})
-  --allowservice          Allow Services (default: ${defaults['allowservice']})
-  --arch                  Architecture (default: ${defaults['arch']})
-  --autoinstalldir        Directory where autoinstall config files are stored on ISO
-  --block                 Block kernel module(s)
-  --bmcip                 BMC/iDRAC IP (default: ${defaults['bmcip']})
-  --bmcpassword           BMC/iDRAC password (default: ${defaults['bmcpassword']})
-  --bmcusername           BMC/iDRAC User (default: ${defaults['bmcusername']})
-  --bootdisk              Boot Disk devices (default: ${defaults['disk']})
-  --bootloader            Boot Loader Location (default: ${defaults['bootloader']})
-  --bootserverfile        Boot sever file (default: ${defaults['bootserverfile']}_BASE)
-  --bootserverip          NFS/Bootserver IP
-  --bootsize              Boot partition size (default: ${defaults['bootsize']})
-  --build                 Type of ISO to build (default: ${defaults['build']})
-  --chrootpackages        List of packages to add to ISO (default: ${defaults['packages']})
-  --cidr                  CIDR (default: ${defaults['cidr']})
-  --codename|--disto      Linux release codename or distribution
-  --compression           Compression algorithm (default: ${defaults['compression']})
-  --country               Country (used for sources.list mirror - default: ${defaults['country']})
-  --debug                 Set debug flag (set -x)
-  --delete                Remove previously created files (default: ${options['force']})
-  --disableservice        Disable Service (default: ${defaults['disableservice']})
-  --diskserial            Disk Serial
-  --diskwwn               Disk WWN
-  --dns                   DNS Server (ddefault: ${defaults['dns']})
-  --enableservice         Enable Service (default: ${defaults['enableservice']})
-  --fallback              Installation fallback (default: ${defaults['fallback']})
-  --firewall              Firewall (default: ${defaults['firewall']})
-  --firstoption           First menu option (default: ${defaults['firstoption']})
-  --gateway               Gateway (default ${defaults['gateway']})
-  --gecos                 GECOS Field Entry (default: ${defaults['disableservice']})
-  --groupts               Groups (default: ${defaults['groups']})
-  --grubfile              GRUB file
-  --grubmenu              Set default grub menu (default: ${defaults['grubmenu']})
-  --grubtimeout           Grub timeout (default: ${defaults['grubtimeout']})
-  --help                  Help/Usage Information
-  --hostname              Hostname (default: ${defaults['hostname']})
-  --inputiso|--vmiso      Input/base ISO file
-  --inputci|--vmci        Input/base Cloud Image file
-  --installmode           Install Mode (default: ${defaults['installmode']})
-  --installmount          Where the install mounts the CD during install (default: ${defaults['installmount']})
-  --installpassword       Temporary install password for remote access during install (default: ${defaults['installpassword']})
-  --installsource         Install Source (default: ${defaults['installsource']})
-  --installtarget         Where the install mounts the target filesystem (default: ${defaults['targetmount']})
-  --installuser           Temporary install username for remote access during install (default: ${defaults['installusername']})
-  --ip                    IP Address (default: ${defaults['ip']})
-  --isolinux              External isolinux file to import
-  --isopackages           List of packages to install (default: ${defaults['packages']})
-  --isourl                Specify ISO URL
-  --isovolid              ISO Volume ID
-  --kernel                Kernel (default: ${defaults['kernel']})
-  --kernelargs            Kernel arguments (default: ${defaults['kernel']}ARGS)
-  --layout|--vmsize       Layout or VM disk size (default: ${defaults['layout']}/${defaults['vmsize']})
-  --lcall                 LC_ALL (default: ${defaults['lcall']})
-  --locale                LANGUAGE (default: ${defaults['locale']})
-  --lvname                LVM Logical Volume Name (default: ${defaults['lvname']})
-  --netmask               Netmask (default: ${defaults['netmask']})
-  --nic|--vmnic           Network device (default: ${defaults['nic']}/${defaults['vmnic']})
-  --oeminstall            OEM Install Type (default: ${defaults['oeminstall']})
-  --oldinputfile          Old release ISO (used with --oldrelease)
-  --oldisourl             Old release ISO URL (used with --oldrelease)
-  --oldrelease            Old release (used for copying file from an older release ISO)
-  --onboot                Enable Network on Boot (default: ${defaults['onboot']})
-  --options               Options (e.g. nounmount, testmode, bios, uefi, verbose, interactive)
-  --outputci              Output Cloud Image file (default: ${defaults['outputcibase']})
-  --outputiso             Output ISO file (default: ${defaults['outputfilebase']})
-  --password              Password (default: ${defaults['password']})
-  --passwordalgorithm     Password Algorithm (default: ${defaults['passwordalgorithm']})
-  --pesize                PE size (default: ${defaults['pesize']})
-  --postinstall           Postinstall action (e.g. installpackages, upgrade, distupgrade, installdrivers, all, autoupgrades)
-  --prefix                Prefix to add to ISO name
-  --preworkdir            Docker work directory (used internally)
-  --pvname                LVM Pysical Volume Name (default: ${defaults['lvname']})
-  --realname              Realname (default ${defaults['realname']})
-  --release               LSB release (default: ${defaults['release']})
-  --rootsize              Root partition size (default: ${defaults['rootsize']})
-  --search                Search output for value (eg --action listallisos --search efi)
-  --selinux               SELinux Mode (default: ${defaults['selinux']})
-  --serialport            Serial Port (default: ${defaults['serialporta']},${defaults['serialportb']})
-  --serialportaddress     Serial Port Address (default: ${defaults['serialportaddressa']},${defaults['serialaddressb']})
-  --serialportspeed       Serial Port Speed (default: ${defaults['serialportspeeda']},${defaults['serialportspeedb']})
-  --sourceid              Source ID (default: ${defaults['sourceid']})
-  --squashfsfile          Squashfs file (default: ${defaults['squashfsfile']}_BASE)
-  --sshkeyfile            SSH key file to use as SSH key (default: ${defaults['maskedsshkeyfile']})
-  --suffix                Suffix to add to ISO name
-  --swapsize|--vmram      Swap or VM memory size (default ${defaults['swapsize']}/${defaults['vmram']})
-  --timezone              Timezone (default: ${defaults['timezone']})
-  --updates               Updates to install (default: ${defaults['updates']})
-  --userdata              Use a custom user-data file (default: generate automatically)
-  --username              Username (default: ${defaults['username']})
-  --version               Display Script Version
-  --vgname                LVM Volume Group Name (default: ${defaults['vgname']})
-  --vmcpus                No ov VM CPUs (default: ${defaults['vmcpus']})
-  --vmname                Set VM name (default: ${defaults['vmname']})
-  --vmtype                VM type (default: ${defaults['vmtype']})
-  --volumemanager         Volume Managers (default: ${defaults['volumemanager']})
-  --workdir               Work directory (default: ${defaults['workdir']})
-  --zfsfilesystems        ZFS filesystems (default: ${defaults['zfsfilesystems']})
-help
+  echo ""
+  echo "Usage: ${script['name']} --action [action] --options [options]"
+  echo ""
+  switchstart="false"
+  while read line; do
+    if [[ "${line}" =~ switchstart ]]; then
+      switchstart="true"
+    fi
+    if [[ "${line}" =~ switchend ]] || [[ "${line}" =~ \* ]]; then
+      switchstart="false"
+    fi
+    if [ "${switchstart}" = "true" ]; then
+      if [[ "${line}" =~ -- ]] && [[ "${line}" =~ [a-z] ]]; then
+        if [[ "${line}" =~ \| ]]; then
+          switch_name=$( echo "${line}" |cut -f1 -d "|" )
+        else
+          switch_name=$( echo "${line}" |cut -f1 -d ")" )
+        fi
+        switch_name=$( echo "${switch_name}" |sed "s/\-\-//g" )
+        switch_name=$( echo "${switch_name}" |sed "s/ //g" )
+        switch_default=$( echo ${defaults[$switch_name]} )
+      fi
+      if [[ "${line}" =~ \# ]] && [[ ! "${line}" =~ switchstart ]]; then
+        if [[ "${switch_name}" =~ [a-z] ]]; then
+          switch_length="${#switch_name}"
+          if [ "${switch_length}" -lt 6 ]; then
+            tab_space="\t\t\t"
+          else
+            if [ "${switch_length}" -lt 14 ]; then
+              tab_space="\t\t"
+            else
+              tab_space="\t"
+            fi
+          fi
+          switch_comment=$( echo "${line}" |cut -f2 -d"#" )
+          if [ ! "${switch_default}" = "" ]; then
+            echo -e  "--${switch_name}${tab_space}${switch_comment} (default: ${switch_default})"
+          else
+            echo -e "--${switch_name}${tab_space}${switch_comment}"
+          fi
+        fi
+      fi
+    fi
+  done < "${script['file']}"
   exit
 }
 
@@ -144,6 +83,7 @@ print_help () {
       ;;
     *)
       print_cli_help
+      exit
       ;;
   esac
 }
